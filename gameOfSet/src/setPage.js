a43b21d22d4c7b022999372f3c63aedf2e7477b5
Abha Naik <43114397+anaik07@users.noreply.github.com>
2020-10-27 12:52:04 -0400

    Update setPage.js

--- gameOfSet/src/setPage.js
+++ gameOfSet/src/setPage.js
@@ -1,29 +1,30 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+class Card {
+  constructor(){
+    this.color = color;
+    this.number = number;
+    this.shape = shape;
+    this.fill = fill;
   }
 }
 
+class Deck {
+  constructor(){
+    this.deck[];
+    const colors = ['Red', 'Green', 'Purple'];
+    const numbers = ['One', 'Two', 'Three'];
+    const shapes = ['Oval', 'Diamond','Squiggle'];
+    const fills = ['Empty', 'Partial', 'Solid'];
+    
+    for (let color in colors){
+      for (let number in numbers){
+        for (let shape in shapes){
+          for (let fill in fills){
+            this.deck.push(new Card(colors[color],numbers[number],shapes[shape],fills[fill]));
+             }
+            }
+           }
+          }
+    }//end constructor
 function replaceThreeCards(deck, setArray, currentBoard) { //When the user passes the deck, the setArray, which holds the indexs of the set to replace, and the current board,
                                                           // this function will remove those cards from the current board and add 3 new cards from the deck
   removeThreeCards(currentBoard, setArray);
