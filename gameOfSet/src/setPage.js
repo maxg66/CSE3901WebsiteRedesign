84934d5c4e30ff56765d29ece5ced7b92f037ae3
anaik07 <naik.89@osu.edu>
2020-10-30 10:12:41 -0400

    rules is dropdown

--- gameOfSet/src/setPage.js
+++ gameOfSet/src/setPage.js
@@ -9,7 +9,7 @@ class Card {
 
 class Deck {
   constructor(){
-    this.deck[];
+    this.deck = new Array();
     const colors = ['Red', 'Green', 'Purple'];
     const numbers = ['One', 'Two', 'Three'];
     const shapes = ['Oval', 'Diamond','Squiggle'];
@@ -25,37 +25,36 @@ class Deck {
            }
           }
     }//end constructor
-}
-
-function loadBoard(deck, currentBoard) {
-  for(let i = 0; i<currentBoard.length; i++){
-    var randomIndexFromDeck = Math.floor(Math.random() * deck.length);
-    var cardToAdd = deck.splice(randomIndexFromDeck,1);
-    currentBoard[i] = cardToAdd;
   }
-}
-
-function printBoard(currentBoard, playerPoints) {
-  //this function can't really be written until we figure out how we want to visually represent cards
-}
-
-function removeSet(currentBoard, setArray) {
-  setArray.forEach(element => {
-    currentBoard[element-1] = null;
-    //remove image/text from actual html page
-  });
-}
-
-function addPoints(playerPoints){
-  if(playerPoints.length>1){
-    //prompt for which player found the set
-    //playerNum = input
-  } else {
-    playerNum = 1;
+  function loadBoard(deck, currentBoard) {
+    for(let i = 0; i<currentBoard.length; i++){
+      var randomIndexFromDeck = Math.floor(Math.random() * deck.length);
+      var cardToAdd = deck.splice(randomIndexFromDeck,1);
+      currentBoard[i] = cardToAdd;
+    }
   }
-  playerPoints[playerNum-1]++;
-
-}
+  
+  function printBoard(currentBoard, playerPoints) {
+    //this function can't really be written until we figure out how we want to visually represent cards
+  }
+  
+  function removeSet(currentBoard, setArray) {
+    setArray.forEach(element => {
+      currentBoard[element-1] = null;
+      //remove image/text from actual html page
+    });
+  }
+  
+  function addPoints(playerPoints){
+    if(playerPoints.length>1){
+      //prompt for which player found the set
+      //playerNum = input
+    } else {
+      playerNum = 1;
+    }
+    playerPoints[playerNum-1]++;
+  
+  }
 
 function replaceThreeCards(deck, setArray, currentBoard) { //When the user passes the deck, the setArray, which holds the indexs of the set to replace, and the current board,
                                                           // this function will remove those cards from the current board and add 3 new cards from the deck
@@ -77,3 +76,19 @@ function removeThreeCards(currentBoard, indexsToRemove) { //When the user passes
       currentBoard.splice(indexsToRemove[i],1);
   }
 }
+
+  //DROPDOWN FUNCTIONALITY//
+  
+  var coll = document.getElementsByClassName("collapsible");
+
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
