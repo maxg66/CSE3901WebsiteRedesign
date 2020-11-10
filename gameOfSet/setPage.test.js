969f339ff8949540a4344d248ab0c242bee960ea
anaik07 <naik.89@osu.edu>
2020-11-09 21:19:55 -0500

    added test cases

--- gameOfSet/setPage.test.js
+++ gameOfSet/setPage.test.js
@@ -1,7 +1,18 @@
 const { TestScheduler } = require('jest');
 const setPage = require('./src/setPage.js').default;
 
+test('no set deck', () => {
+    fullDeck.printBoard()
+    expect(Deck.length).toBe(15);
+})
+
+test('print board', () => {
+    fullDeck.addThreeCards()
+    expect(Deck.length).toBe(12);
+})
+
 test('initialize card', () => {
     var card = new Card('red,', 'one', 'oval', 'empty');
     expect(card.printCard).toBe('one red oval with a fill of empty');
 })
+
